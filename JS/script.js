80e5008ded6e7c8c2872ddcce5779333725902da
Erick Ard <erickardi07@gmail.com>
2023-09-13 21:36:18 +0700

    Website XainnzCoffe

---
+++ JS/script.js
@@ -0,0 +1,70 @@
+// Toggle class active Hamburger Menu
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = (e) => {
+    navbarNav.classList.toggle('active');
+    e.preventDefault();
+};
+
+// Toggle class active Search Menu
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Toggle class active Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+};
+
+// Klik luar di element
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button')
+
+
+document.addEventListener('click', function(e) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+})
+
+
+// Close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+// Klick luar
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+};
